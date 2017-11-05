0ca4792a5d71af41afa29d8c70b65ad21fd9a9cf
Jacek Wieczorek <mrjjot@gmail.com>
2017-11-05 15:38:45 +0100

    Remove old declarations

--- src/osc.h
+++ src/osc.h
@@ -9,8 +9,6 @@ extern const volatile uint8_t PROGMEM samples[][SAMPLE_LEN];
 #define SAMPLE_COUNT ( sizeof( samples ) / sizeof( samples[0] ) )
 #endif
 
-extern volatile uint8_t noteon, notenum;
-
 extern void oscinit( );
 extern void oscset( uint16_t compv );
 extern void ldsample( const volatile uint8_t *sample, uint8_t vol );
